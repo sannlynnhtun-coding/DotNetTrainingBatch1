526a7154ee9342eb1f259433eb8386adbb8c38f2
Hnin Phyu Phyu Aung <hninphyuphyuaung16@gmail.com>
2023-12-21 19:32:41 +0630

    [Complete] Apex Chart {Scatter Chart}

---
+++ AEHKLMNSTZDotNetCore.MvcApp/Models/ApexChartScatterChartModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AEHKLMNSTZDotNetCore.MvcApp.Models
+{
+    public class ApexChartScatterChartModel
+    {
+        public string name { get; set; }
+        public double[][] data { get; set; }
+
+        //public List<List<float>> data { get; set; }
+    }
+}
