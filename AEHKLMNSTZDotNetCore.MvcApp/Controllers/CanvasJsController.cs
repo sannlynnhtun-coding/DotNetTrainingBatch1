2422ee482fc78def3ca404f4beb792968592e7a9
Zin Ko Ko <zinkoko@acedatasystems.com>
2023-12-23 21:44:16 +0630

    [Complete] Inverted Funnel Chart

--- AEHKLMNSTZDotNetCore.MvcApp/Controllers/CanvasJsController.cs
+++ AEHKLMNSTZDotNetCore.MvcApp/Controllers/CanvasJsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AEHKLMNSTZDotNetCore.MvcApp.Models;
 
 namespace AEHKLMNSTZDotNetCore.MvcApp.Controllers
 {
@@ -7,6 +8,23 @@ namespace AEHKLMNSTZDotNetCore.MvcApp.Controllers
         public IActionResult PieChart()
         {
             return View();
+        }
+
+        public IActionResult InvertedFunnelChart()
+        {
+            var model = new CanvasJsInvertedFunnelChartModel
+            {
+                DataPoints = new List<DataPoint>
+                {
+                    new DataPoint {Y = 3500, Label = "Impressions"},
+                    new DataPoint {Y = 2130, Label = "Clicked"},
+                    new DataPoint {Y = 1950, Label = "Free Downloads"},
+                    new DataPoint {Y = 500, Label = "Purchase"},
+                    new DataPoint {Y = 300, Label = "Renewal"},
+
+                }
+            };
+            return View(model);
         }
     }
 }
