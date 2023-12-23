7357933aa700c4bc6030d9581d0e0949acae6c0c
Zin Ko Ko <zinkoko@acedatasystems.com>
2023-12-23 23:04:45 +0630

    [Complete] SemiCircleDonut Chart

---
+++ AEHKLMNSTZDotNetCore.MvcApp/Models/HighChartSemiCircleDonutChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AEHKLMNSTZDotNetCore.MvcApp.Models
+{
+    public class HighChartSemiCircleDonutChartModel
+    {
+        public string Title {  get; set; }
+        public double Point { get; set; }
+    }
+}
