1af2e2d05bea12212db8a87232ea155e84a1d9df
Thet Wai Aung <twa453354@gmail.com>
2023-12-26 21:35:22 +0630

    [Complete] ChartJs AreaChart

--- AEHKLMNSTZDotNetCore.MvcApp/Controllers/ChartJsController.cs
+++ AEHKLMNSTZDotNetCore.MvcApp/Controllers/ChartJsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AEHKLMNSTZDotNetCore.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AEHKLMNSTZDotNetCore.MvcApp.Controllers
 {
@@ -8,10 +9,26 @@ namespace AEHKLMNSTZDotNetCore.MvcApp.Controllers
         {
             return View();
         }
-
         public IActionResult HorizontalBarChart()
         {
             return View();
+        }
+        public IActionResult AreaChart()
+        {
+            ChartJsAreaChartResponseModel model = new ChartJsAreaChartResponseModel();
+            List<ChartJsAreaChartModel> areaChartList = new List<ChartJsAreaChartModel>()
+            {
+                new ChartJsAreaChartModel() {Month="January",Improve=20,Reduce=0},
+            new ChartJsAreaChartModel() { Month = "February", Improve = 60, Reduce = 0 },
+            new ChartJsAreaChartModel() { Month = "March", Improve = 0, Reduce = -20 },
+            new ChartJsAreaChartModel() { Month = "April", Improve = 40, Reduce = 0 },
+            new ChartJsAreaChartModel() { Month = "May", Improve = 0, Reduce = -40 },
+            new ChartJsAreaChartModel() { Month = "June", Improve = 0, Reduce = -20 },
+            new ChartJsAreaChartModel() { Month = "July", Improve = 30, Reduce = 0 }
+        };
+            model.Data = areaChartList;
+
+            return View(model);
         }
     }
 }
