428e7a129e2c3d68f192194e29266c6f4c108a1a
Khant Min Thu <kmin01405@gmail.com>
2023-12-27 21:33:47 +0630

    [complete] doughnut chart in chartjs

---
+++ AEHKLMNSTZDotNetCore.MvcApp/Models/ChartJsDoughnutChartResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AEHKLMNSTZDotNetCore.MvcApp.Models
+{
+    public class ChartJsDoughnutChartResponseModel
+    {
+        public int DataCount { get; set; }
+
+        public List<string> Labels { get; set; }
+
+        public List<ChartJsDoughnutChartModel> DataSet { get; set; }
+    }
+}
