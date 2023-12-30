fce103780c487874fc32c38b8d195c0760b4ba96
Hnin Phyu Phyu Aung <hninphyuphyuaung16@gmail.com>
2023-12-30 20:12:55 +0630

    [Complete] ChartJs {Mulit Axis Line Chart}

---
+++ AEHKLMNSTZDotNetCore.MvcApp/Models/ChartJsMultiAxisLineChartResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AEHKLMNSTZDotNetCore.MvcApp.Models
+{
+	public class ChartJsMultiAxisLineChartResponseModel
+	{
+		public int DataCount { get; set; }
+		public List<string> Labels { get; set; }
+		public List<ChartJsMultiAxisLineChartModel> Datasets { get; set; }
+	}
+}
