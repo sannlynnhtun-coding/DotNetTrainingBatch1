f4ca6cbed7bab03d2ecf5890887ac70d6ed71db3
ZinLinnhtet <mackk5504@gmail.com>
2024-01-14 18:14:59 +0630

    [Complete]Refit and RestApi CRUD

--- AEHKLMNSTZDotNetCore.MvcApp/Controllers/BlogRefitController.cs
+++ AEHKLMNSTZDotNetCore.MvcApp/Controllers/BlogRefitController.cs
@@ -1,21 +1,59 @@
 ﻿using AEHKLMNSTZDotNetCore.MvcApp.Interfaces;
+using AEHKLMNSTZDotNetCore.MvcApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AEHKLMNSTZDotNetCore.MvcApp.Controllers
 {
-    public class BlogRefitController : Controller
-    {
-        private readonly IBlogApi _blogApi;
+	public class BlogRefitController : Controller
+	{
+		private readonly IBlogApi _blogApi;
 
-        public BlogRefitController(IBlogApi blogApi)
-        {
-            _blogApi = blogApi;
-        }
+		public BlogRefitController(IBlogApi blogApi)
+		{
+			_blogApi = blogApi;
+		}
+		[HttpGet]
+		[ActionName("Index")]
+		public async Task<IActionResult> Index()
+		{
+			var model = await _blogApi.GetBlogs();
+			return View(model);
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            var model = await _blogApi.GetBlogs();
-            return View(model);
-        }
-    }
+		[ActionName("Create")]
+		public IActionResult BlogRefitCreate()
+		{
+			return View("BlogRefitCreate");
+		}
+
+		[HttpPost]
+		[ActionName("Save")]
+		public async Task<IActionResult> BlogSave(BlogDataModel reqModel)
+		{
+			var model = await _blogApi.CreateBlog(reqModel);
+			return Redirect("/blogrefit");
+		}
+
+		[ActionName("Edit")]
+		public async Task<IActionResult> BlogRefitEdit(int id)
+		{
+			var model = await _blogApi.GetBlog(id);
+			return View("BlogRefitEdit", model);
+		}
+
+		[HttpPost]
+		[ActionName("Update")]
+		public async Task<IActionResult> BlogUpdate(int id, BlogDataModel reqModel)
+		{
+			var model = await _blogApi.UpdateBlog(id, reqModel);
+			return Redirect("/blogrefit");
+		}
+
+		[ActionName("Delete")]
+		public async Task<IActionResult> BlogDelete(int id)
+		{
+			var model = await _blogApi.DeleteBlog(id);
+			return Redirect("/blogrefit");
+		}
+	}
 }
--- AEHKLMNSTZDotNetCore.MvcApp/Program.cs
+++ AEHKLMNSTZDotNetCore.MvcApp/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
